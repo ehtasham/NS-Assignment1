bed0e0b8d13763cf8dac0394c762818c62eb52c6
ehtasham <ehtashamawan@gmail.com>
2018-03-03 21:59:43 +0500

    port scanner

---
+++ PortScan.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import socket
+import sys
+from datetime import datetime
+
+remoteServer=str(sys.argv[1])
+remoteServerIP  = socket.gethostbyname(remoteServer)
+
+
+# Check what time the scan started
+t1 = datetime.now()
+
+# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+
+# We also put in some error handling for catching errors
+for port in range(1,2):  
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((remoteServerIP, port))
+    if result == 0:
+        print "Port {}: 	 Open".format(port)
+    sock.close()
+
+
+# Checking the time again
+t2 = datetime.now()
+
+# Calculates the difference of time, to see how long it took to run the script
+total =  t2 - t1
+
+# Printing the information to screen
+print 'Scanning Completed in: ', total
