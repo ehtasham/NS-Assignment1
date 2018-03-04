12e7149c3848f3e502f0d377f4529a810fcc0390
ehtasham <ehtashamawan@gmail.com>
2018-03-04 15:30:24 +0500

    port scan

--- PortScan.py
+++ PortScan.py
@@ -2,30 +2,31 @@
 import socket
 import sys
 from datetime import datetime
+import time
 
+
+total_ports=23
+if (len(sys.argv) !=2) :
+	print "enter the name of remoteServer"
+	exit()
 remoteServer=str(sys.argv[1])
 remoteServerIP  = socket.gethostbyname(remoteServer)
 
-
-# Check what time the scan started
-t1 = datetime.now()
-
-# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
-
-# We also put in some error handling for catching errors
-for port in range(1,20):  
+t1 = time.time()
+for port in range(0,total_ports):  
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((remoteServerIP, port))
+    
     if result == 0:
-        print "Port {}: 	 Open".format(port)
+    	service_name=socket.getservbyport(port)
+        # print ('Port %d: Open  Service: %s' (port,service_name))
+        print('Port: %s Open  Service :  %s  ' % \
+              (port,service_name))
     sock.close()
 
+t2 = time.time()
+time_difference =  t2 - t1
+scan_rate=total_ports/time_difference
 
-# Checking the time again
-t2 = datetime.now()
-
-# Calculates the difference of time, to see how long it took to run the script
-total =  t2 - t1
-
-# Printing the information to screen
-print 'Scanning Completed in: ', total
+print ('Scanning Completed in: %s seconds '% time_difference)
+print ('Scan Rate: %s  '% scan_rate)
